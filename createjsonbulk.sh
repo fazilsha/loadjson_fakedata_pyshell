aef640458558e7943d04c9fcc97e6c493b019fd8
fazilsha <fazilsha1197@gmail.com>
2021-07-07 23:38:37 +0530

    code recorrect

--- createjsonbulk.sh
+++ createjsonbulk.sh
@@ -2,12 +2,15 @@
 mkdir Bulkjson
 cd Bulkjson
 echo "Number of json files to load:"
-read $num_json
+read num_json
 echo "Installing python faker library...."
 pip install faker
 echo "Files getting created: "
-python createjson.py $num_json
+python ../createjson.py $num_json
 echo "Json load over..Tranferring files to bulkjson folder.."
-for file in *.json; mv $file /Bulkjson; done
+for file in ../*.json
+do
+ mv $file ../Bulkjson
+done
 echo "Moved Everything...All set"
 
