b074f9f8c91018d7f1952d6760b783843731a700
fazilsha <fazilsha1197@gmail.com>
2021-07-08 21:11:19 +0530

    Updating pre requistes

--- createjsonbulk.sh
+++ createjsonbulk.sh
@@ -3,8 +3,6 @@ mkdir Bulkjson
 cd Bulkjson
 echo "Number of json files to load:"
 read num_json
-echo "Installing python faker library...."
-pip install faker
 echo "Files getting created: "
 python ../createjson.py $num_json
 echo "Json load over..Tranferring files to bulkjson folder.."
---
+++ install_prequiste.sh
@@ -0,0 +1,3 @@
+sudo apt-get install python3
+sudo apt-get install pip
+sudo pip install faker
