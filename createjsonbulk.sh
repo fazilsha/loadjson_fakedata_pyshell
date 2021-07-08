f8d3d2ac6c4eee17586c9615a53c8e5486bc25d2
fazilsha <fazilsha1197@gmail.com>
2021-07-08 12:33:25 -0400

    Update

--- createjsonbulk.sh
+++ createjsonbulk.sh
@@ -5,10 +5,5 @@ echo "Number of json files to load:"
 read num_json
 echo "Files getting created: "
 python ../createjson.py $num_json
-echo "Json load over..Tranferring files to bulkjson folder.."
-for file in ../*.json
-do
- mv $file ../Bulkjson
-done
-echo "Moved Everything...All set"
+echo "Created $num_json fake json data"
 
